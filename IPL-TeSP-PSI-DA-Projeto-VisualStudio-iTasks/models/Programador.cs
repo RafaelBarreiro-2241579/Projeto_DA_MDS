964f8fbb53afceb8e9d428244e800a407225c120
BeatrizzDiass <2241609@my.ipleiria.pt>
2025-06-25 22:57:28 +0100

    GereUtilizadores

--- IPL-TeSP-PSI-DA-Projeto-VisualStudio-iTasks/models/Programador.cs
+++ IPL-TeSP-PSI-DA-Projeto-VisualStudio-iTasks/models/Programador.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using iTasks.models;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace iTasks.models
 {
@@ -23,6 +24,24 @@ namespace iTasks.models
         public NivelExperiencia NivelExperiencia { get; set; }
         public List<Tarefa> Tarefas { get; set; }
 
-    }
 
+        public Programador()
+        {
+
+        }
+
+        public Programador (string nome, string username, string password, NivelExperiencia nivelExperiencia, Gestor idGestor)
+        {
+            this.Nome = nome;
+            this.Username = username;
+            this.Password = password;
+            this.NivelExperiencia = nivelExperiencia;
+            this.IdGestor = idGestor.Id;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} | Username: {Username} | Experiencia: {NivelExperiencia}";
+        }
+    }
 }
